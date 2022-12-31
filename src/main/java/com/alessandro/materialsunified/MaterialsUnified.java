1e94f02f52552fd6b869687c4210af5491cdf70f
Kessy2105 <ale.casale08@gmail.com>
2022-12-31 13:30:07 +0100

    Added a comment

--- src/main/java/com/alessandro/materialsunified/MaterialsUnified.java
+++ src/main/java/com/alessandro/materialsunified/MaterialsUnified.java
@@ -18,6 +18,7 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
+// Add a comment
 @Mod(MaterialsUnified.MOD_ID)
 public class MaterialsUnified
 {
